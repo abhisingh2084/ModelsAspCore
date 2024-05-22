b4055f72363c3951200bc8fc8242910b2c46a56a
abhishek kumar <abhisingh2084@gmail.com>
2024-05-22 11:51:40 +0530

    first commit

--- ModelsAspCore/Controllers/HomeController.cs
+++ ModelsAspCore/Controllers/HomeController.cs
@@ -17,11 +17,11 @@ namespace ModelsAspCore.Controllers
         {
             var students = new List<StudentModel> 
             { 
-                new StudentModel {RollNo = 1, Name = "Abhishek Kumar", Gender = "Male", Standard = 5},
-                new StudentModel {RollNo = 2, Name = "Aryan Raj", Gender = "Male", Standard = 5},
-                new StudentModel {RollNo = 3, Name = "Vikash Ojha", Gender = "Male", Standard = 5},
-                new StudentModel {RollNo = 4, Name = "Vinay Singh", Gender = "Male", Standard = 5},
-                new StudentModel {RollNo = 5, Name = "Prakash Yadav", Gender = "Male", Standard = 5},
+                new StudentModel {RollNo = 1, Name = "Abhishek", Gender = "Male", Standard = 5},
+                new StudentModel {RollNo = 2, Name = "Aryan Raj", Gender = "Male", Standard = 6},
+                new StudentModel {RollNo = 3, Name = "Vikash Ojha", Gender = "Male", Standard = 7},
+                new StudentModel {RollNo = 4, Name = "Vinay Singh", Gender = "Male", Standard = 8},
+                new StudentModel {RollNo = 5, Name = "Prakash Yadav", Gender = "Male", Standard = 1},
             };
 
             ViewData["StudentsList"] = students;
